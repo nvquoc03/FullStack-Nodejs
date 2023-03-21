5a473ee81188e7989c7462ad3799a04fb2a5a390
Van Quoc <quocx2003@gmail.com>
2023-03-21 19:16:37 +0700

    nodejs in fullstack course

---
+++ src/route/web.js
@@ -0,0 +1,27 @@
+import express from "express";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", homeController.getHomePage);
+
+    router.get("/crud", homeController.getCRUD); // Tạo
+
+    router.post("/post-crud", homeController.postCRUD); // Trang khi tạo xong
+
+    router.get('/get-crud', homeController.displayGetCRUD); // Hiện thị
+
+    router.get('/edit-crud', homeController.getEditCRUD); // Sửa
+
+    router.post('/put-crud', homeController.putCRUD); // Trang khi sửa xog
+
+    router.get('/delete-crud', homeController.deleteCRUD); // Xóa
+
+    router.post('/api/login', userController.handleLogin);
+
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
