7b24005d4aa587bf1e1f8894824e04eee6830ec6
Sherlock Nguyen <quocx2003@gmail.com>
2023-04-17 23:37:45 +0700

    video74-BulkCreateToSaveAllAtOnce

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,4 +1,9 @@
 import db from "../models/index"
+require('dotenv').config();
+import _ from 'lodash'
+
+const MAX_NUMBER_SCHEDULE = process.env.MAX_NUMBER_SCHEDULE;
+
 
 let getTopDoctorHome = (limitInput) => {
     return new Promise(async (resolve, reject) => {
@@ -135,9 +140,66 @@ let getDetailDoctorById = (inputId) => {
     })
 }
 
+let bulkCreateScheduleService = (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!data && !data.arrSchedule || !data.doctorId || !data.FormatedDate) {
+                resolve({
+                    errCode: 1,
+                    errMessage: 'Missing required parameter!'
+                })
+            } else {
+                let schedule = data.arrSchedule
+                if (schedule && schedule.length > 0) {
+                    schedule = schedule.map(item => {
+                        item.maxNumber = MAX_NUMBER_SCHEDULE;
+                        return item;
+                    })
+                }
+
+                //Get all existing data
+                let existing = await db.Schedule.findAll({
+                    where: { doctorId: data.doctorId, date: data.FormatedDate },
+                    attributes: ['doctorId', 'date', 'timeType', 'maxNumber'],
+                    raw: true
+                });
+
+                //Convert TimeStamp(00:00:00.000Z) to TimeStamp (012345678)
+                if (existing && existing.length > 0) {
+                    existing = existing.map(item => {
+                        item.date = new Date(item.date).getTime();
+                        return item;
+                    })
+                }
+
+                //Existing in DataBase, Schedule is input Client
+                //Compare different 
+                let toCreate = _.differenceWith(schedule, existing, (a, b) => {
+                    return a.timeType === b.timeType && a.date === b.date;
+                });
+
+                //Create data if have different
+                if (toCreate && toCreate.length > 0) {
+                    await db.Schedule.bulkCreate(toCreate)
+                }
+
+                resolve({
+                    errCode: 0,
+                    errMessage: "0K!"
+                })
+
+            }
+
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
     getTopDoctorHome: getTopDoctorHome,
     getAllDoctors: getAllDoctors,
     saveDetailInforDoctor: saveDetailInforDoctor,
-    getDetailDoctorById: getDetailDoctorById
+    getDetailDoctorById: getDetailDoctorById,
+    bulkCreateScheduleService: bulkCreateScheduleService
 }
