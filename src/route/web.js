5ec340e26c56e45d90075655a643ee9e164f0315
Sherlock Nguyen <quocx2003@gmail.com>
2023-05-05 11:49:21 +0700

    video91-Viết API lấy tất cả chuyên khoa ở DataBase

--- src/route/web.js
+++ src/route/web.js
@@ -37,6 +37,8 @@ let initWebRoutes = (app) => {
     router.post('/api/verify-book-appointment', patientController.postVerifyBookAppointment);
 
     router.post('/api/create-new-specialty', specialtyController.createSpecialty);
+    router.get('/api/get-all-specialties', specialtyController.getAllSpecialty);
+
 
 
 
